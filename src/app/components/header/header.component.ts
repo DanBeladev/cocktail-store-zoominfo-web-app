80bfa3712b6ebe2fcaef78bf686ea30b5ccbdbc5
Dan <danbeladev@gmail.com>
2020-03-28 16:25:30 +0300

    environment urls

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  public homeUrl = environment.CLIENT_URL;
-  public purchasesUrl = `${environment.CLIENT_URL}/purchases`;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,9 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const URL = {
-  HEROKU_URL: 'https://cocktail-store-backend.herokuapp.com',
-  DEV_URL: 'http://localhost:3000',
-  CLIENT_DEV_URL: 'http://localhost:4200'
-};
-
 export const environment = {
   production: false,
-  API_URL: URL.DEV_URL,
-  CLIENT_URL: URL.CLIENT_DEV_URL,
+  API_URL: 'http://localhost:3000',
 };
 
 /*
